858e6b64579ab30ba1134156e1c02cfda9111913
vaishdotcom <vaishali.raja@student.uts.edu.au>
2022-09-04 22:09:05 +1000

    80% complete

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,6 +7,7 @@ public class InputManager : MonoBehaviour
     private Transform[] transArray;
     private GameObject redObj;
     private GameObject blueObj;
+    private float yPosition;
   
     // Start is called before the first frame update
     void Start()
@@ -27,6 +28,20 @@ public class InputManager : MonoBehaviour
         {
            transArray[0].transform.Rotate(0.0f, 0.0f, 45.0f, Space.Self);
            transArray[1].transform.Rotate(0.0f, 0.0f, -45.0f, Space.Self);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 redPos = transArray[0].transform.position;
+            Vector3 bluePos = transArray[1].transform.position;
+            Vector3 tempPos;
+
+            tempPos.y = redPos.y;
+            redPos.y = bluePos.y;
+            bluePos.y = tempPos.y;
+
+            transArray[0].transform.position = redPos;
+            transArray[1].transform.position = bluePos;
         }
     }
 }
